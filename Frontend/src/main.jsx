9f011e82e57c68fb8e92bf4f03a7a22c3994c4af
RajatXAI <webdevrar@gmail.com>
2026-06-26 00:32:03 +0530

    feat(styles): add global styles and css reset

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "../src/routes/router.jsx";
-import "../src/styles/main.scss";
+import "./shared/styles/main.scss";
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
